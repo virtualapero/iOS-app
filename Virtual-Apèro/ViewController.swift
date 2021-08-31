1d8a2117efa97717802a0564da7d85e3dc217c76
Felix Alexander Kaube <support@kaube-felix.de>
2021-08-31 15:04:53 +0200

    Initial Commit

---
+++ Virtual-Apèro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual-Apèro
+//
+//  Created by Felix Kaube on 31.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Virtual-ApèroTests/Virtual_Ape_roTests.swift
@@ -0,0 +1,33 @@
+//
+//  Virtual_Ape_roTests.swift
+//  Virtual-ApèroTests
+//
+//  Created by Felix Kaube on 31.08.21.
+//
+
+import XCTest
+@testable import Virtual_Ape_ro
+
+class Virtual_Ape_roTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
